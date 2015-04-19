06b49006bc7df7fcba11eab59396ce0b9a84799d
Lucas <lucashen@gmail.com>
2015-04-19 09:29:06 -0300

    using  with firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,27 @@
  * Controller of the angularJsFirebaseApp
  */
 angular.module('angularJsFirebaseApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $timeout) {
     var rootRef = new Firebase('https://glowing-torch-4779.firebaseio.com/');
     var childRef = rootRef.child('message');
+
+    childRef.on('value', function(snapshot){
+      $timeout(function(){
+        var snapshotVal = snapshot.val();
+        console.log(snapshotVal);
+        $scope.message = snapshotVal;
+      });
+    });
+
+    $scope.$watch('message.text', function(newVal) {
+      if(!newVal){
+        return;
+      }
+
+      childRef.update({
+        text: newVal
+      });
+    });
 
     $scope.setMessage = function() {
       childRef.set({
