9812d22d00024ff7313804789eec94b509c12a23
Lucas <lucashen@gmail.com>
2015-04-20 20:16:02 -0300

    child changed, Off and Once

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,16 @@ angular.module('angularJsFirebaseApp')
   .controller('MainCtrl', function ($scope, $timeout) {
     var rootRef = new Firebase('https://glowing-torch-4779.firebaseio.com/');
     var messagesRef = rootRef.child('messages');
+    var titleRef = rootRef.child('title');
 
+    $scope.title = null;
     $scope.currentUser = null;
     $scope.currentText = null;
     $scope.messages = [];
+
+    titleRef.once('value', function(snapshot) {
+      $scope.title = snapshot.val();
+    });
 
     messagesRef.on('child_added', function(snapshot){
       $timeout(function(){
@@ -23,9 +29,50 @@ angular.module('angularJsFirebaseApp')
         // console.log(snapshot.name());
         // console.log(snapshot.numChildren());
         var snapshotVal = snapshot.val();
-        $scope.messages.push(snapshotVal);
+        $scope.messages.push({
+          text: snapshotVal.text,
+          user: snapshotVal.user,
+          name: snapshot.key()
+        });
       });
     });
+
+    messagesRef.on('child_changed', function(snapshot){
+      $timeout(function(){
+        var snapshotVal = snapshot.val();
+        var message = findMessageByName(snapshot.key());
+        message.text = snapshotVal.text;
+      });
+    });
+
+    messagesRef.on('child_removed', function(snapshot){
+      $timeout(function(){
+        deleteMessageByName(snapshot.key());
+      });
+    });
+
+    function findMessageByName(name){
+      var messageFound = null;
+
+      for(var i = 0; i < $scope.messages.length; i++){
+        var currentMessage = $scope.messages[i];
+        if(currentMessage.name === name){
+          messageFound = currentMessage;
+          break;
+        }
+      }
+      return messageFound;
+    }
+
+    function deleteMessageByName(name){
+      for(var i = 0; i < $scope.messages.length; i++){
+        var currentMessage = $scope.messages[i];
+        if(currentMessage.name === name){
+          delete $scope.messages[i];
+          break;
+        }
+      }
+    }
 
     $scope.sendMessage = function(){
       var newMessage = {
@@ -35,6 +82,10 @@ angular.module('angularJsFirebaseApp')
 
       messagesRef.push(newMessage);
     }
+
+    $scope.turnFeedOff = function() {
+      messagesRef.off();
+    };
 
 
     /*
